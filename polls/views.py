7ef6936514142ed81ddca1c225a8dd815a718e2e
Phimbun Issawilanon <s6201012620198@email.kmutnb.ac.th>
2021-04-30 16:30:39 +0700

    show Question

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
 
-def index(requst):
- return HttpResponse("hi")
+from .models import Question
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
