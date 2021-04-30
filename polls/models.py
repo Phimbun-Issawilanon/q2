22deafc88a05cd1556d1fe4bd8283725514a2073
Phimbun Issawilanon <s6201012620198@email.kmutnb.ac.th>
2021-04-30 15:50:24 +0700

    crate models

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+#คำถามกับตัวเลือก
+class Questions(models.Model):
+    question = models.CharField(max_lenght = 250 )
+    pub_date = models.DateTimeField('datepublished')
+
+class Choice(models.Model):
+    choice = models.CharField(max_lenght = 250 )
+    
